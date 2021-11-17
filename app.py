837359b188045b6a5558281e3add6b2df37079a4
P. Sampson <psampdev@gmail.com>
2021-11-16 23:25:29 -0500

    Add /query endpoint that can accept query_id in JSON format; set up temp architechture for calling models and utils

--- app.py
+++ app.py
@@ -1,10 +1,24 @@
-from flask import Flask
+from models import sentiment, political
+from utils import retrieve, write
+from flask import Flask, json, request, jsonify
+
 app = Flask(__name__)
 
 @app.route('/')
 def index():
   return 'Server Works!'
   
-@app.route('/greet')
-def say_hello():
-  return 'Hello from Server'
+@app.route('/query', methods=['POST'])
+def query():
+    # extract query from request and read from database
+    query_id = request.get_json()["query_id"]
+    query_data = retrieve.fetch(query_id)
+
+    # run models on query data
+    sentiment_results = sentiment.run(query_data)
+    political_results = political.run(query_data)
+
+    # store results
+    write.store(sentiment_results, political_results)
+
+    return jsonify(query_id)
---
+++ utils/retrieve.py
@@ -0,0 +1,6 @@
+# import pandas
+import requests
+
+# read query csv & bag of words from webscraping and return dataframe
+def fetch(query_id):
+    pass
