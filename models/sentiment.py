81ba1bb837d60d0469c4f7ae71994f4925e46186
P. Sampson <psampdev@gmail.com>
2021-11-23 10:39:08 -0500

    Firebase reads/writes

--- models/sentiment.py
+++ models/sentiment.py
@@ -1,2 +1,26 @@
-def run(data):
+import nltk
+nltk.download('vader_lexicon')
+from nltk.sentiment.vader import SentimentIntensityAnalyzer
+
+sid = SentimentIntensityAnalyzer()#takes a string and returns one of 4 categories: negative, positive, neutral, and compound
+
+class SentimentAnalysis():
+    def __init__(self, text):
+        self.polarity = sid.polarity_scores(text)
+    def getSentiment(self):
+        self.sent_message = "The article was rated as " + str(self.polarity['neg']*100) + "% NEGATIVE"
+        self.sent_message += "\nThe article was rated as " + str(self.polarity['neu']*100) + "% NEUTRAL"
+        self.sent_message += "\nThe article was rated as " + str(self.polarity['pos']*100) + "% POSITIVE"
+        self.sentiment = ''
+        if self.polarity['compound'] <= -0.05:
+            self.sentiment = "NEGATIVE"
+        elif self.polarity['compound'] > -0.05 and self.polarity['compound'] < 0.05:
+            self.sentiment = "NEUTRAL"
+        else:
+            self.sentiment = "POSITIVE"
+        self.sent_message += "\nThe article's overall sentiment is " + self.sentiment
+        
+        return self.sent_message
+
+def run(clean_text):
     pass
