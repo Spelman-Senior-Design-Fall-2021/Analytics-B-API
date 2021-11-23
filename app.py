c13f0d70d90f70e47701f154c6d0c9d88c6e32c2
P. Sampson <psampdev@gmail.com>
2021-11-23 11:54:57 -0500

    Add political model

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from models import sentiment  # , political
+from models import sentiment, political
 from firebase_admin import delete_app
 from utils import write
 from flask import Flask, json, request, jsonify
@@ -43,7 +43,8 @@ def run_models(id, data):
       text = d["clean_text"]
       url = d["url"]
       results["sentiment"].append({"url": url})
+      results["political"].append({"url": url})
       results["sentiment"].append(sentiment.run(text))
-      # results["political"] = political.run(text)
+      results["political"].append(political.run(text))
 
     return results
